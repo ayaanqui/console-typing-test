c983b32d3ad6d8a19f26b09e0b00751a4d30f3e4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-04-16 23:00:39 -0500

    Generate paragraph

--- src/main/java/com/github/ayaanqui/consoleTypingTest/App.java
+++ src/main/java/com/github/ayaanqui/consoleTypingTest/App.java
@@ -1,7 +1,52 @@
 package com.github.ayaanqui.consoleTypingTest;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class App {
+    private static String filePath = "./src/main/resources/";
+
+    private static Set<String> addWordsFromFiles(String... files) {
+        Set<String> allWords = new HashSet<>();
+
+        Arrays.stream(files).forEach(file -> {
+            try {
+                Scanner s = new Scanner(new File(filePath + file));
+                while (s.hasNext())
+                    allWords.add(s.next());
+                s.close();
+            } catch (Exception e) {
+            }
+        });
+        return allWords;
+    }
+
+    private static String generateParagraph(Set<String> words, int minSize, int maxSize, boolean specailChars,
+            int size) {
+        Stream<String> reducedWords = words.stream().filter(w -> {
+            return w.length() >= minSize && w.length() <= maxSize;
+        });
+
+        StringBuilder paragraph = new StringBuilder();
+        Iterator<String> it = reducedWords.iterator();
+        for (int i = 0; i < size; i++) {
+            paragraph.append(it.next());
+            paragraph.append(" ");
+        }
+        return paragraph.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println("**Typing Test Program**");
+
+        Set<String> words = addWordsFromFiles("words_alpha.txt", "words.txt", "words_english.txt");
+        System.out.println("Total words loaded: " + words.size() + "\n");
+
+        System.out.println(generateParagraph(words, 1, 8, false, 50));
     }
 }
